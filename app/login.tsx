39d7cddfefa242438d6e24ffbbee3d72298b1452
Max A <corp.workMA@gmail.com>
2025-05-21 21:48:51 +0300

    [dev-0001]: add auth routing

---
+++ app/login.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const LoginScreen = () => {
+  return (
+    <View>
+      <Text>LOgin Screen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
